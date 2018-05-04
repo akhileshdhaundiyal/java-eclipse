b53401b3d0e026628c645e981a2def56f8d01aca
Training <Training@192.168.252.64>
2018-05-04 12:18:32 +0530

    Calculator V 1.0

---
+++ GitExampleProject/src/com/niit/Calculator.java
@@ -0,0 +1,33 @@
+package com.niit;
+
+public class Calculator {
+
+	Integer a,b;	
+	
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+
+	public Calculator(Integer a, Integer b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
